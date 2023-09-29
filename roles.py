4516b08695f75c63a8b077ebf00cf9a1dda1b902
yuval <matasaro@sheridancollege.ca>
2023-09-29 08:43:32 -0400

    initial

---
+++ roles.py
@@ -0,0 +1,8 @@
+strength = 0
+health = 0
+intelligence = 0
+
+wizard = [strength - 2, health - 1, intelligence + 2]
+warrior = [strength + 2, health + 2, intelligence - 2]
+
+print("random quest here") 

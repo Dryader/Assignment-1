03ec0408bcdbeea44a47d4403170ce4a3fdbb265
Dryader <75398625+Dryader@users.noreply.github.com>
2023-10-04 13:59:19 -0400

    intros and file structure

---
+++ App.py
@@ -0,0 +1,12 @@
+print("Welcome to the quest for epic loot!")
+print("You have 3 challenges in order to get the loot.")
+print("You also have 2 possible roles to choose from.")
+
+
+
+print("You come across a group of goblins on your travels. They look like they are up to no good. What do you do?")
+print("1. Attack the goblins")
+print("2. Run away")
+print("3. Try to talk to the goblins")
+print("4. Do nothing")
+choice = input("Enter your choice: ")
---
+++ Role2.py
@@ -0,0 +1,4 @@
+strength = 0
+health = 0
+intelligence = 0
+wizard = [strength - 2, health - 1, intelligence + 2]

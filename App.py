6a0c1dc25ec2f1293a866835e9d14ffc1a6c1897
Dryader <75398625+Dryader@users.noreply.github.com>
2023-10-04 16:57:56 -0400

    added winloss function

--- App.py
+++ App.py
@@ -23,3 +23,10 @@ def rules():
 
     elif roll() <= 12:
         print("You have succeeded the challenge and have gained an attribute point")
+
+        
+def winloss(result1, result2, result3):
+    if result1 == "win" and result2 == "win" and result3 == "win":
+        print("You have won the game, congratulations!")
+    else:
+        print("You have lost the game, better luck next time!")

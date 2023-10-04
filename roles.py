85e60221ad44b0ad35060ce50962dd5b708f0867
Dryader <75398625+Dryader@users.noreply.github.com>
2023-10-04 13:23:57 -0400

    test commit

--- roles.py
+++ roles.py
@@ -6,3 +6,4 @@ wizard = [strength - 2, health - 1, intelligence + 2]
 warrior = [strength + 2, health + 2, intelligence - 2]
 
 print("random quest here") 
+print("The wizard has", wizard, "points")
